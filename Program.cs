2b504e6c52d07aba1daf572901d09a7038c12ba7
Tking <tuffdodge86@gmail.com>
2026-04-02 10:22:10 -0500

    The Code for the Classes assignment

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            The_Welcome welcome = new The_Welcome();// Creates the "The_Welcome" Object 
+            welcome.Welcome(); // calls the welcome method from The_Welcome odject 
+
+            Console.WriteLine("What is your name User ? ");// Asks the user for there name 
+            string userName = Console.ReadLine();// Stores the name in userName 
+
+            welcome.Hello(userName); // Calls the Hello method and adds the user name to the argunment to it 
         }
     }
 }
---
+++ The_Welcome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Dodge_Tuff
+{
+    internal class The_Welcome 
+    {
+
+        public void Welcome() // Wellcome's Methods 
+        {
+            Console.WriteLine(" Hello User");// says hello to the user 
+        }
+
+        public void Hello(string name) // Hello's Methods 
+        {
+            Console.WriteLine($"{name}, Thank you for your participation in giving your name to me "); // thanks the user for giving there name 
+        }
+
+    }
+}
