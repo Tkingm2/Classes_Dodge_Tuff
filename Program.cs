6608da1c8fa997181e9940089b01a623fdf20edd
Tking <tuffdodge86@gmail.com>
2026-04-02 09:32:07 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes_Dodge_Tuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
